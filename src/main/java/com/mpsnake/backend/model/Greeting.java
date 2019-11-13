f646174eafdfa8866634f420dc7e9c424230da1f
Roy <roy.appeldoorn@gmail.com>
2019-11-13 14:09:24 +0100

    Initial commit

---
+++ src/main/java/com/mpsnake/backend/model/Greeting.java
@@ -0,0 +1,15 @@
+package com.mpsnake.backend.model;
+
+public class Greeting {
+
+    private String content;
+    public Greeting() {
+    }
+    public Greeting(String content) {
+        this.content = content;
+    }
+    public String getContent() {
+        return content;
+    }
+
+}
