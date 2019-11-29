30ed8cc6135dee43cc4b73f96a053b31ae0ed0eb
Roy <roy.appeldoorn@gmail.com>
2019-11-29 17:20:23 +0100

    added logic

--- src/main/java/com/mpsnake/backend/model/Snake.java
+++ src/main/java/com/mpsnake/backend/model/Snake.java
@@ -1,20 +1,54 @@
 package com.mpsnake.backend.model;
 
 import org.springframework.web.socket.WebSocketSession;
+import com.mpsnake.backend.utils.SnakeUtils;
 
 import java.awt.*;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Deque;
+
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
 
 public class Snake {
 
     private static final int DEFAULT_LENGTH = 5;
 
+    private final int id;
+    private final SessionConnectedEvent session;
+
     private Direction direction;
     private int length = DEFAULT_LENGTH;
     private Location head;
     private final Deque<Location> tail = new ArrayDeque<Location>();
+    private final String hexColor;
 
+    public Snake(int id, SessionConnectedEvent session) {
+        this.id = id;
+        this.session = session;
+        this.hexColor = SnakeUtils.getRandomHexColor();
+        resetState();
+    }
+
+    private void resetState() {
+        this.direction = Direction.NONE;
+        this.head = SnakeUtils.getRandomLocation();
+        this.tail.clear();
+        this.length = DEFAULT_LENGTH;
+    }
+
+    public synchronized Location getHead() {
+        return head;
+    }
+
+    public synchronized Collection<Location> getTail() {
+        return tail;
+    }
+
+    public int getId() {
+        return id;
+    }
 
 }
--- src/main/java/com/mpsnake/backend/websocket/controller/WebSocketController.java
+++ src/main/java/com/mpsnake/backend/websocket/controller/WebSocketController.java
@@ -1,21 +1,41 @@
 package com.mpsnake.backend.websocket.controller;
 
-import com.google.gson.Gson;
 import com.mpsnake.backend.model.*;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
-import org.springframework.web.util.HtmlUtils;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Controller
 public class WebSocketController {
 
+    private ArrayList<String> socketConnections;
+
+    private static Timer gameTimer = null;
+
+    private static final long TICK_DELAY = 100;
+
+    private static final ConcurrentHashMap<Integer, Snake> snakes =
+            new ConcurrentHashMap<Integer, Snake>();
+
+    private static final AtomicInteger snakeIds = new AtomicInteger(0);
+
+    private final int id;
+    private Snake snake;
+
+    public WebSocketController() {
+        this.id = snakeIds.getAndIncrement();
+    }
+
     @MessageMapping("/update")
     @SendTo(value = "/topic/location")
     private Location send(Location location) throws Exception {
+
         return new Location(location.getX()+1, location.getY()+1);
     }
 
@@ -25,8 +45,34 @@ public class WebSocketController {
 //        return new Location(100, 20);
 //    }
 
-//    @EventListener
-//    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
-//
-//    }
+    public static synchronized void addSnake(Snake snake) {
+        if (snakes.size() == 0) {
+            startTimer();
+        }
+        snakes.put(Integer.valueOf(snake.getId()), snake);
+    }
+
+    public static Collection<Snake> getSnakes() {
+        return Collections.unmodifiableCollection(snakes.values());
+    }
+
+    public static void startTimer() {
+        gameTimer = new Timer(WebSocketController.class.getSimpleName() + " Timer");
+        gameTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    tick();
+                } catch (Throwable e) {
+                    log.error("Caught to prevent timer from shutting down", e);
+                }
+            }
+        }, TICK_DELAY, TICK_DELAY);
+    }
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+        this.snake = new Snake(id, event);
+        addSnake(snake);
+    }
 }
