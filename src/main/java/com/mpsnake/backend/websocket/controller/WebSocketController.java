61932242883f8990017a46433118af4d669a608e
royappeldoorn <roy.appeldoorn@gmail.com>
2020-01-04 14:25:52 +0100

    Added websocket endpoint for joining user

--- src/main/java/com/mpsnake/backend/websocket/controller/WebSocketController.java
+++ src/main/java/com/mpsnake/backend/websocket/controller/WebSocketController.java
@@ -1,7 +1,10 @@
 package com.mpsnake.backend.websocket.controller;
 
+import com.mpsnake.backend.logic.IGameLogic;
+import com.mpsnake.backend.model.Snake;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpAttributesContextHolder;
@@ -12,17 +15,20 @@ public class WebSocketController {
 
     private static final Log log = LogFactory.getLog(WebSocketController.class);
 
+    @Autowired
+    private IGameLogic gameLogic;
+
     @MessageMapping("/update")
-    @SendTo(value = "/topic/location")
+    @SendTo(value = "/topic/public")
     public String newPositions(String msg) throws Exception {
         return msg;
     }
 
-//    @MessageMapping("/game.addUser")
-//    @SendTo(value = "/topic/public")
-//    public void addUser(String username) throws Exception {
-//        String sessionId = SimpAttributesContextHolder.currentAttributes().getSessionId();
-//        Snake newSnake = new Snake(sessionId, username);
-//        gameLogic.addSnake(newSnake);
-//    }
+    @MessageMapping("/addUser")
+    @SendTo(value = "/topic/public")
+    public void addUser(String username) throws Exception {
+        String sessionId = SimpAttributesContextHolder.currentAttributes().getSessionId();
+        Snake newSnake = new Snake(sessionId, username);
+        gameLogic.addSnake(newSnake);
+    }
 }
