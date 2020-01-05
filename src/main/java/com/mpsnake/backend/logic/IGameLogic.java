0ee3874b148b68c1287f9de05ec6af070959b30d
royappeldoorn <roy.appeldoorn@gmail.com>
2020-01-05 17:05:13 +0100

    Added method to set the snake direction

--- src/main/java/com/mpsnake/backend/logic/IGameLogic.java
+++ src/main/java/com/mpsnake/backend/logic/IGameLogic.java
@@ -1,5 +1,6 @@
 package com.mpsnake.backend.logic;
 
+import com.mpsnake.backend.model.Direction;
 import com.mpsnake.backend.model.Snake;
 
 public interface IGameLogic {
@@ -7,4 +8,6 @@ public interface IGameLogic {
     void addSnake(Snake snake);
 
     void removeSnake(String id);
+
+    void setDirection(String id, Direction direction);
 }
