748e5bffb9dc99870c109940301837189ad9f5c8
royappeldoorn <roy.appeldoorn@gmail.com>
2020-01-09 14:42:30 +0100

    Moved playfield logic to its own class

---
+++ src/main/java/com/mpsnake/backend/utils/Playfield.java
@@ -0,0 +1,19 @@
+package com.mpsnake.backend.utils;
+
+public class Playfield {
+    private static final int GRID_SIZE = 20;
+    private static final int PLAYFIELD_WIDTH = 1100;
+    private static final int PLAYFIELD_HEIGHT = 900;
+
+    public static int getPlayFieldWidth() {
+        return PLAYFIELD_WIDTH;
+    }
+
+    public static int getPlayFieldHeight() {
+        return PLAYFIELD_HEIGHT;
+    }
+
+    public static int getGridSize() {
+        return GRID_SIZE;
+    }
+}
