86a7719262773b7726f24c4b1397b8600c2640fc
royappeldoorn <roy.appeldoorn@gmail.com>
2020-01-19 14:50:25 +0100

    Added test file

---
+++ src/test/java/com/mpsnake/backend/ModelTests.java
@@ -0,0 +1,15 @@
+package com.mpsnake.backend;
+
+import com.mpsnake.backend.models.Location;
+import org.junit.jupiter.api.Test;
+
+public class ModelTests {
+
+    // <editor-fold defaultstate="collapsed" desc="Location test">
+    @Test
+    public void createNewPlayerWithCorrectData() {
+        Location location = new Location();
+    }
+
+    // </editor-fold>
+}
