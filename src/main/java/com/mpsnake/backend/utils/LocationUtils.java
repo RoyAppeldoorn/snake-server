26a195b3460cea149054e7d4b6eb9aba167c841a
royappeldoorn <roy.appeldoorn@gmail.com>
2020-01-19 14:49:50 +0100

    Refactored to use static properties

--- src/main/java/com/mpsnake/backend/utils/LocationUtils.java
+++ src/main/java/com/mpsnake/backend/utils/LocationUtils.java
@@ -9,15 +9,15 @@ public class LocationUtils {
     private static final Random random = new Random();
 
     public static Location getRandomLocation() {
-        int x = roundByGridSize(random.nextInt(PlayfieldUtils.getPlayFieldWidth()));
-        int y = roundByGridSize(random.nextInt(PlayfieldUtils.getPlayFieldHeight()));
+        int x = roundByGridSize(random.nextInt(PlayfieldUtils.PLAYFIELD_WIDTH));
+        int y = roundByGridSize(random.nextInt(PlayfieldUtils.PLAYFIELD_HEIGHT));
         return new Location(x, y);
     }
 
     private static int roundByGridSize(int value) {
-        value = value + (PlayfieldUtils.getGridSize() / 2);
-        value = value / PlayfieldUtils.getGridSize();
-        value = value * PlayfieldUtils.getGridSize();
+        value = value + (PlayfieldUtils.GRID_SIZE / 2);
+        value = value / PlayfieldUtils.GRID_SIZE;
+        value = value * PlayfieldUtils.GRID_SIZE;
         return value;
     }
 }
